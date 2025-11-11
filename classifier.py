2c84fc652b4fff1e15b3196da170e0729cfcdbf9
Lê Thanh Lâm <lamlethanh177@gmail.com>
2025-11-11 21:57:04 +0700

    fix: add `inplace=True`

--- classifier.py
+++ classifier.py
@@ -15,7 +15,7 @@ class SimpleClassifier(nn.Module):
         layers = [
             weight_norm(nn.Linear(in_dim, hid_dim), dim=None),
             activation_func,
-            nn.Dropout(args.dropout, inplace=True),
+            nn.Dropout(args.dropout, inplace=False),
             weight_norm(nn.Linear(hid_dim, out_dim), dim=None)
         ]
         self.main = nn.Sequential(*layers)
